1b11a25f80ec86420acb82c1f5dba3cbef6a07c4
Alexander Saltykov <a070783@rambler.ru>
2017-03-25 19:53:00 +0300

    hm6

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,12 @@ loaders.push({
 });
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        towns: './src/towns.js'
+    },
     output: {
-        filename: '[hash].js',
+        filename: '[chunkhash].js',
         path: './dist'
     },
     devtool: 'source-map',
@@ -24,15 +27,22 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
-        sourceMap: true,
-        compress: {
-        drop_debugger: false
-        }
+            sourceMap: true,
+            compress: {
+                drop_debugger: false
+            }
         }),
         new ExtractTextPlugin('styles.css'),
         new HtmlPlugin({
-            title: 'Loft School sample project',
-            template: 'index.hbs'
+            title: 'Main Homework',
+            template: 'index.hbs',
+            chunks: ['main']
+        }),
+        new HtmlPlugin({
+            title: 'Div Drag And Drop',
+            template: 'towns.hbs',
+            filename: 'towns.html',
+            chunks: ['towns']
         }),
         new CleanWebpackPlugin(['dist'])
     ]
